3673242d4b0542134ec3930538f8a4db2cf3f745
Berthy Feng <bfeng@princeton.edu>
2018-10-23 21:12:20 -0400

    Update models.py

--- models.py
+++ models.py
@@ -11,5 +11,8 @@ class Category(models.Model):
 
 class Event(models.Model):
     org = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    datetime = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    location = models.CharField(max_length=100)  # Better field type?
+    is_free = models.BooleanField()
