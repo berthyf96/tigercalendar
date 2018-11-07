d56f971a0b1f40228cae60dc0360b6b1c45b7e5b
Rebecca <rb25@princeton.edu>
2018-11-06 19:38:36 -0500

    Update tigercal/settings.py

--- tigercal/settings.py
+++ tigercal/settings.py
@@ -36,28 +36,28 @@ ALLOWED_HOSTS = ['https://whatsroaring.herokuapp.com/'] # Change this later
 INSTALLED_APPS = [
     'calendarapp.apps.CalendarappConfig',
     #'schedule',
-    #'djangobower',
-    #'django.contrib.admin',
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes',
-    #'django.contrib.sessions',
-    #'django.contrib.messages',
+    # 'djangobower',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
 BOWER_INSTALLED_APPS = (
-    'jquery'
+    'jquery',
     #'bootstrap'
 )
 
 MIDDLEWARE = [
-    #'django.middleware.security.SecurityMiddleware',
-    #'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'tigercal.urls'
