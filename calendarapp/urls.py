046530e153024c7b8b474ff96fd930cc75b0f117
Rebecca <rb25@princeton.edu>
2018-11-08 16:41:44 -0500

    test

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-#app_name = 'calendarapp'
+app_name = 'calendarapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('cal', views.CalView.as_view(), name='cal'),
